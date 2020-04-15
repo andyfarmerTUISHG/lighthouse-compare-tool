f5d9ddb5c9661186392f078a08dda36ec387fa60
Farmer, Andy <Andy.Farmer@specialistholidays.com>
2020-04-15 20:35:21 +0100

    feat: Add Launch Chrome function - New function that accepts URL as a parameter - Launch Chome, and send to URL parameter

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const lighthouse = require("lighthouse");
 const chromeLauncher = require("chrome-launcher");
 
 console.log("Light House Tool");
+
+const launchChrome = (url) => {
+  chromeLauncher.launch({
+    startingUrl: url,
+  });
+};
