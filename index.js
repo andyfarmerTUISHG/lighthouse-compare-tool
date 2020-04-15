8c5d848a0593bb47c54b7f78848f8a3728672a75
Farmer, Andy <Andy.Farmer@specialistholidays.com>
2020-04-15 20:46:51 +0100

    feat: Use Chrome Launcer Promise - As the functions returns a promise, we can access the chrome object - Output the Chrome object - Close the launched Chrome instance after 3 Seconds

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const chromeLauncher = require("chrome-launcher");
 console.log("Light House Tool");
 
 const launchChrome = (url) => {
-  chromeLauncher.launch({
-    startingUrl: url,
-  });
+  chromeLauncher
+    .launch({
+      startingUrl: url,
+    })
+    .then((chrome) => {
+      console.log(chrome);
+      setTimeout(() => chrome.kill(), 3000);
+    });
 };
 
 launchChrome("http://akf0676.github.io/");
