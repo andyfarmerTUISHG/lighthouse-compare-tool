8f4e3f79ef63898c811463846adef955fc0ea9e9
Farmer, Andy <Andy.Farmer@specialistholidays.com>
2020-04-18 09:40:37 +0100

    feat: add saving of reports - Update to readme of actions to Save Report - refactor url constant to urlParam - Create new URL Object inorder to define folder directory - Create reports folder and maintain with .gitkeep - Update gitignore to ignore contents of reports directory - Update Index to take into account any path parameters

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const lighthouse = require("lighthouse");
 const chromeLauncher = require("chrome-launcher");
 const argv = require("yargs").argv;
+const url = require("url");
+const fs = require("fs");
 
+const urlParam = argv.url;
+if (urlParam) {
+  const urlObj = new URL(urlParam);
+  let directoryName = `reports/ ${urlObj.hostname.replace("www.", "")}`;
+
+  if (urlObj.pathname !== "/") {
+    //if the URL has a pathname, to replace slashes with underscores
+    directoryName = directoryName + urlObj.pathname.replace(/\//g, "_");
+  }
+  if (!fs.existsSync(directoryName)) {
+    fs.mkdirSync(directoryName);
+  }
+}
 console.log("Light House Tool");
 console.log(`----------------------------`);
-const url = argv.url;
 
 const launchChromeAndRunLighthouse = (url) => {
   return chromeLauncher.launch().then((chrome) => {
@@ -17,10 +31,12 @@ const launchChromeAndRunLighthouse = (url) => {
   });
 };
 
-if (url) {
-  launchChromeAndRunLighthouse(url).then((results) => {
+if (urlParam) {
+  launchChromeAndRunLighthouse(urlParam).then((results) => {
     console.log(results);
   });
 } else {
-  throw `You have not passed a URL to Lighthouse \n eg node index.js --url xxx`;
+  console.log(
+    `You have not passed a URL to Lighthouse \n eg node index.js --url xxx`
+  );
 }
--- readme.md
+++ readme.md
@@ -8,6 +8,28 @@ In order to run the tool:-
 
 `node index.js --url http://akf0676.github.io/`
 
+## Saving the Reports
+
+To avoid reports from different websites getting mixed up we will organise like this:-
+
+```ASCII
+project
+│  index.js
+└───reports
+    │
+    └───domain1.com
+    │   │   dateTtime.json
+    │   │   dateTtime.json
+    │
+    └───domain2.com
+        │   dateTtime.json
+        │   dateTtime.json
+
+
+```
+
+Reports will be named with a timestamp so no two reports will be the same, and will enable the ability to distinguish between reports.
+
 ## Changes
 
 Within this project supporting conventional commits.
